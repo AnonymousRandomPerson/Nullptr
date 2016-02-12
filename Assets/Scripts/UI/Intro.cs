58427ca14417f38483296a78cdae786b4d3c34b9
Jonathan Hunter <hunterjonathant@gmail.com>
2016-02-12 09:13:37 -0500

    added intro songs

--- Assets/Scripts/UI/Intro.cs
+++ Assets/Scripts/UI/Intro.cs
@@ -5,15 +5,23 @@ namespace Assets.Scripts.UI
     class Intro : MonoBehaviour
     {
         public GameObject fakeTitle, Error, StartButton;
+        public Util.SoundPlayer introSongs;
 
         void Start()
         {
             Managers.GameManager.Intro();
+            introSongs.loop = true;
+            introSongs.loopSong = 0;
+            introSongs.PlaySong(0);
         }
 
         public void FakePlay()
         {
             Error.SetActive(true);
+            introSongs.Stop();
+            introSongs.loop = true;
+            introSongs.loopSong = 1;
+            introSongs.PlaySong(1);
         }
 
         public void ErrorClick()
@@ -22,6 +30,7 @@ namespace Assets.Scripts.UI
             Error.SetActive(false);
             StartButton.SetActive(true);
             Managers.GameManager.Run();
+            introSongs.Stop();
         }
 
         public void Play()

0a2d5f63e8c8595883460680e587e716e2f5ddd6
Niandong <tianxu2009@gmail.com>
2016-03-24 11:01:43 -0400

    Created the Fan and the enemy that approaches you, and the jumps to explode in midair

---
+++ Assets/Scripts/Enemy/Enemies/ApproachAndJumpEnemy.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using Assets.Scripts.Managers;
+using Assets.Scripts.Util;
+
+namespace Assets.Scripts.Enemy.Enemies
+{
+    class ApproachAndJumpEnemy : Enemy
+    {
+        [SerializeField]
+        private float speed;
+
+        // This much distance, or below, then jump. Check only x-axis distance.
+        [SerializeField]
+        private float spaceToJump;
+
+        //Jump vector
+        private Vector2 jumpVec = new Vector2(-40, 60);
+
+        //location of player, to know when to jump
+        private Transform playerLocTrans;
+
+        //How many seconds from jump until explode
+        [SerializeField]
+        private float timeToExplode = 2;
+
+        //Counts down to explode
+        private float explodeTimer;
+
+        //In air or not
+        private bool inAir;
+
+        //RigidBody, to be used for applying jump vector
+        Rigidbody2D rBody;
+
+        public override void InitData()
+        {
+            base.InitData();
+            playerLocTrans = FindObjectOfType<Managers.PlayerManager>().GetPlayer().transform;
+			Debug.Log ("Got player transform as " + playerLocTrans);
+            rBody = GetComponent<Rigidbody2D>();
+        }
+
+        public override void RunEntity()
+        {
+            base.RunEntity();
+            if (!inAir)
+            {
+                transform.Translate(GetForward() * speed * Time.deltaTime);
+            }
+
+            if ((explodeTimer -= Time.deltaTime) <= 0)
+            {
+                Debug.Log("Destroyed");
+                //Create explosion with value 2 to hurt player
+				//for now use 0
+				ExplosionManager.instance.SpawnExplosion(2, transform, Enums.Direction.None);
+				Debug.Log("I HAVE EXPLODED");
+                Destroy(this);
+            }
+        }
+
+        void FixedUpdate()
+        {
+            if (Mathf.Abs(playerLocTrans.position.x - ((Vector2)transform.position).x) <= spaceToJump
+                && !inAir)
+            {
+                inAir = true;
+                rBody.AddForce(jumpVec, ForceMode2D.Impulse);
+                explodeTimer = timeToExplode;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Platforms/Fan.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Platforms
+{
+    //This fan only blows upwards
+
+    class Fan : MonoBehaviour
+    {
+        //Magnitude of fan's blowing force.
+		[SerializeField]
+        private float blowbackMagnitude;
+
+        void applyForce (Collider2D col)
+        {
+            //Change this method if there are any enemies
+            //that are supposed to be still, like sedentary enemies
+			col.attachedRigidbody.AddForce(blowbackMagnitude * Vector2.up, ForceMode2D.Force);
+        }
+
+        void OnTriggerEnter2D (Collider2D col)
+        {
+            applyForce(col);
+        }
+
+        void OnTriggerStay2D (Collider2D col)
+        {
+			
+            applyForce(col);
+        }
+    }
+}

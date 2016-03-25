86610c21b54accb98331c92ff0987a9763cb403e
Jonathan Hunter <hunterjonathant@gmail.com>
2016-03-25 10:30:40 -0400

    Intro basically done

---
+++ Assets/Scripts/Enemy/Enemies/IntroEnemy.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Enemy.Enemies
+{
+    class IntroEnemy : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject cutscene;
+
+        void OnTriggerEnter2D(Collider2D coll)
+        {
+            Debug.Log("A");
+            if (Managers.GameManager.IsRunning)
+            {
+                Debug.Log("B");
+                if (coll.gameObject.tag == "PlayerBullet")
+                {
+                    Debug.Log("C");
+                    if (cutscene != null)
+                        cutscene.SetActive(true);
+                }
+            }
+        }
+    }
+}

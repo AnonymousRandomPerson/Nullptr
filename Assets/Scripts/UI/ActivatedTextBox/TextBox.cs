9b7b2a0838c42568527126f3f035af7137286c74
Atra <sharr@derpymail.org>
2016-04-01 15:28:16 -0400

    Created TextBox. TextBoxes are boxes with text inside them

---
+++ Assets/Scripts/UI/ActivatedTextBox/TextBox.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UI.ActivatedTextBox {
+    public class TextBox : MonoBehaviour {
+
+        [SerializeField]
+        private Image image;
+        [SerializeField]
+        private Text text;
+
+    }
+}
